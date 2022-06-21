6c986e7f0bfce0a03958fade91d315a85435d1d6
Harsha Saranam <harshasaranam@gmail.com>
2022-06-20 20:27:50 -0700

    change in 5 min interval

--- src/com/doordash/StartEndTimeInterval.java
+++ src/com/doordash/StartEndTimeInterval.java
@@ -6,65 +6,73 @@ import java.util.List;
 import java.util.Map;
 
 public class StartEndTimeInterval {
-    static class Time {
-        int day = -1;
-        int hour = 0;
-        int min = 0;
-        boolean am = false;
+    class Time {
+        int day;
+        int hour;
+        int min;
+        boolean dayTime;
 
-        public Time(int day, int hour, int min, boolean am) {
+        Time(int day, int hour, int min, boolean isDay) {
             this.day = day;
             this.hour = hour;
             this.min = min;
-            this.am = am;
+            this.dayTime = isDay;
         }
 
-        void add(int mins) {
+        private void add(int mins) {
             hour += (mins + min) / 60;
             min = (mins + min) % 60;
-            if (hour >= 13) {
-                am = !am;
-                hour = hour % 12;
-                if (am)
-                    day += 1;
+            if (hour == 12) {
+                dayTime = !dayTime;
+                hour = 0;
+                if (dayTime) {
+                    day = (day + 1) % 7;
+                }
             }
         }
 
-        int getNumeric() {
-            return (((day * 100) + hour) * 100) + min;
+        private int getNumeric() {
+            int addHour = hour;
+            if (format.equals("24 HOUR")) {
+                addHour = dayTime ? hour : hour + 12;
+            }
+
+            int dayAndHour = (day * 100) + addHour;
+            return (dayAndHour * 100) + min;
         }
 
-        boolean equals(Time t2) {
+        private boolean equals(Time t2) {
             return day == t2.day &&
                     hour == t2.hour &&
                     min == t2.min &&
-                    Boolean.compare(am, t2.am) == 0;
+                    Boolean.compare(dayTime, t2.dayTime) == 0;
         }
     }
 
-    static Map<String, Integer> mapDays = new HashMap<>();
+    private Map<String, Integer> mapDays = new HashMap<>();
+    private String format;
 
-    static List<Integer> getIntervals(String start, String end) {
+    private List<Integer> getIntervals(String start, String end, String format) {
+        this.format = format;
         mapDays();
         List<Integer> intervals = new ArrayList<>();
         Time startTime = getTime(start);
         Time endTime = getTime(end);
         while (!startTime.equals(endTime)) {
             startTime.add(5);
-       //     System.out.println(startTime.getNumeric());
             intervals.add(startTime.getNumeric());
         }
         return intervals;
     }
 
-    static Time getTime(String time) {
+    private Time getTime(String time) {
         String[] info = time.split(" ");
         String[] hrMin = info[1].split(":");
-        boolean am = info[2].equals("am");
-        return new Time(mapDays.get(info[0]), Integer.parseInt(hrMin[0]), Integer.parseInt(hrMin[1]), am);
+        boolean dayTime = info[2].equals("am");
+        return new Time(mapDays.get(info[0]), Integer.parseInt(hrMin[0]), Integer.parseInt(hrMin[1]), dayTime);
     }
 
-    static void mapDays() {
+    private void mapDays() {
         mapDays.put("mon", 1);
         mapDays.put("tue", 2);
         mapDays.put("wed", 3);
@@ -75,8 +83,17 @@ public class StartEndTimeInterval {
     }
 
     public static void main(String[] args) {
-        for (Integer interval : getIntervals("mon 10:00 pm", "mon 11:00 pm")) {
-            System.out.println("->" + interval);
-        }
+        StartEndTimeInterval startEndTimeInterval = new StartEndTimeInterval();
+        List<Integer> data = startEndTimeInterval.getIntervals("mon 10:00 pm", "tue 11:00 pm", "12 HOUR");
+        System.out.println(data.size());
+        System.out.println(data);
+
+        List<Integer> data2 = startEndTimeInterval.getIntervals("mon 10:00 pm", "tue 11:00 pm", "24 HOUR");
+        System.out.println(data2.size());
+        System.out.println(data2);
+
+        List<Integer> data3 = startEndTimeInterval.getIntervals("sun 10:00 pm", "mon 11:00 pm", "24 HOUR");
+        System.out.println(data3.size());
+        System.out.println(data3);
     }
 }
